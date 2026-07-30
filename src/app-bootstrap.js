3334a49029f323a29892c1aa6ca8ce90b766fd1f
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-07-30 17:38:24 -0400

    Carregar sincronização antes do aplicativo

---
+++ src/app-bootstrap.js
@@ -0,0 +1,9 @@
+import { initializeWorkoutCloudSync } from './workout-cloud-sync.js';
+
+try {
+  await initializeWorkoutCloudSync();
+} catch (error) {
+  console.error('MaYFiT: sincronização inicial indisponível:', error);
+}
+
+await import('./main.jsx');
