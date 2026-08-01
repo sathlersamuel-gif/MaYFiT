759b6a463273c521fddbd2e3afaf743c56bb68fd
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-07-31 22:50:03 -0400

    Remover remontagem contínua que fazia tela do aluno piscar

--- src/admin-exercise-thumbnails.js
+++ src/admin-exercise-thumbnails.js
@@ -1,7 +1,6 @@
 import './body-evolution.js?v=3';
 import './body-evolution-save-fix.js?v=3';
 import './body-evolution-delete-record.js?v=2';
-import './student-navigation-guard.js?v=4';
 import './workout-return-refresh.js?v=2';
 
 const MAYFIT_EXERCISE_DB='https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json';
