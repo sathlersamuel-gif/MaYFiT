38f6ac48d804d925cbbd0aa2e7b5b685f2f5e602
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-07-31 22:15:12 -0400

    Ativa atualização automática ao voltar do treino

--- src/admin-exercise-thumbnails.js
+++ src/admin-exercise-thumbnails.js
@@ -2,6 +2,7 @@ import './body-evolution.js';
 import './body-evolution-save-fix.js';
 import './body-evolution-delete-record.js';
 import './student-navigation-guard.js';
+import './workout-return-refresh.js';
 
 const MAYFIT_EXERCISE_DB='https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json';
 const MAYFIT_IMAGE_BASE='https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/';
