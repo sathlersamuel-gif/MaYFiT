ea24ea0456ee01bf90adf8e8d75419c7ada88358
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-08-01 11:45:55 -0400

    Remover carregamentos indiretos e conflitantes da área do aluno

--- src/admin-exercise-thumbnails.js
+++ src/admin-exercise-thumbnails.js
@@ -1,9 +1,3 @@
-import './body-evolution.js?v=3';
-import './body-evolution-save-fix.js?v=3';
-import './body-evolution-delete-record.js?v=2';
-import './student-performance-fix.js?v=2';
-import './body-evolution-preserve.js?v=1';
-
 const MAYFIT_EXERCISE_DB='https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json';
 const MAYFIT_IMAGE_BASE='https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/';
 let mayfitExerciseMap=null;
