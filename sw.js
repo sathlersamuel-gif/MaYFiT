14f735de21e921dbc703320cbad6094a9cac8cd9
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-08-04 16:10:40 -0400

    Deixa abertura do treino instantânea

--- sw.js
+++ sw.js
@@ -1,22 +1,41 @@
-const VERSION='mayfit-sw-v24-workout-name-direct-load';
+const VERSION='mayfit-sw-v25-instant-workout';
+const EXERCISE_IMAGE_CACHE='mayfit-exercise-images-v1';
+
+function isExerciseImage(request){
+  if(request.destination!=='image')return false;
+  const url=new URL(request.url);
+  return url.hostname==='raw.githubusercontent.com'&&url.pathname.includes('/yuhonas/free-exercise-db/');
+}
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key))))
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==EXERCISE_IMAGE_CACHE).map(key=>caches.delete(key))))
   );
 });
 
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
     const keys=await caches.keys();
-    await Promise.all(keys.map(key=>caches.delete(key)));
+    await Promise.all(keys.filter(key=>key!==EXERCISE_IMAGE_CACHE).map(key=>caches.delete(key)));
     await self.clients.claim();
   })());
 });
 
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
+
+  if(isExerciseImage(event.request)){
+    event.respondWith((async()=>{
+      const cache=await caches.open(EXERCISE_IMAGE_CACHE);
+      const cached=await cache.match(event.request);
+      if(cached)return cached;
+      const response=await fetch(event.request);
+      if(response.ok||response.type==='opaque')cache.put(event.request,response.clone());
+      return response;
+    })());
+    return;
+  }
 
   event.respondWith((async()=>{
     try{
