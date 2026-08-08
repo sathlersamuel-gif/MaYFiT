7fdc74d391abdc478ccdeb250db51299531da30a
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-08-08 08:06:50 -0400

    Fix Android navigation bar overlap across app

--- android/app/src/main/java/com/mayfit/app/MainActivity.java
+++ android/app/src/main/java/com/mayfit/app/MainActivity.java
@@ -1,6 +1,11 @@
 package com.mayfit.app;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -9,5 +14,18 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(NativeTtsPlugin.class);
         super.onCreate(savedInstanceState);
+
+        View webView = bridge.getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets navigationBars = windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars());
+            view.setPadding(
+                view.getPaddingLeft(),
+                view.getPaddingTop(),
+                view.getPaddingRight(),
+                navigationBars.bottom
+            );
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
     }
 }
